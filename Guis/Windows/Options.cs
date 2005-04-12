a0a9e7e2336b87e1896704069fdc8a76fab34c15
cypher23 <cypher23@3f652342-c2f2-0310-9621-dc9b3910fd50>
2005-04-12 19:28:34 +0000

    woops... missing files :)

---
+++ Guis/Windows/Options.cs
@@ -0,0 +1,22 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+#endregion
+
+namespace AbbotForWindows
+{
+	partial class Options : Form
+	{
+		public Options()
+		{
+			InitializeComponent();
+		}
+	}
+}
