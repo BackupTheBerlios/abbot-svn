c0dc23433d5e28a38463da9e0f6fa0fe591bd0ca
saxx <saxx@3f652342-c2f2-0310-9621-dc9b3910fd50>
2005-04-30 17:55:39 +0000

    some minor changes (GPL header etc) i forgot.

--- Guis/Service/Program.cs
+++ Guis/Service/Program.cs
@@ -1,6 +1,23 @@
-using System.Collections.Generic;
+/*
+Abbot: The petite IRC bot
+Copyright (C) 2005 The Abbot Project
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 using System.ServiceProcess;
-using System.Text;
 
 namespace Abbot {
 	static class Program {
